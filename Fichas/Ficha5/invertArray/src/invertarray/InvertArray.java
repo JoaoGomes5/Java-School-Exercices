3c1dc72c14cfcb7e7dbf8e33fc54b06821a8cd20
unknown <joaopfg.2002@gmail.com>
2020-11-27 21:36:40 +0000

    6

--- Fichas/Ficha5/invertArray/src/invertarray/InvertArray.java
+++ Fichas/Ficha5/invertArray/src/invertarray/InvertArray.java
@@ -1,21 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package invertarray;
+import java.util.Scanner;
 
-/**
- *
- * @author João Gomes
- */
 public class InvertArray {
+    
+    static int[] readVector(int n){
+        Scanner sc = new Scanner(System.in);
+        int[] vector = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            System.out.println("Insira o valor da " + (i+1) + " posição do vetor");
+            vector[i] = sc.nextInt();
+        }
+        
+        return vector;
 
-    /**
-     * @param args the command line arguments
-     */
+    }
+    
+     static void invertArray(int[] vector, int n){
+         System.out.println("Vector invertido");
+         
+        for (int i = n - 1; i >= 0 ; i--) {
+            System.out.print(vector[i]);
+            if(i != 0){
+                System.out.print(" , "); 
+            }
+        }
+        
+         System.out.println("");
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+        
+        System.out.println("Insira o tamanho do vextor");
+        n = sc.nextInt();
+        
+       int []vector = readVector(n);
+            
+        invertArray(vector,n);
     }
     
 }
